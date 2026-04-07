bb330a6f7c216403e8a3a3048c0f32008f5fe180
eijei? <ajagodera2003@gmail.com>
2026-04-08 06:57:23 +0800

    Add files via upload

---
+++ Forms/FingerprintRegistrationForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace BAMS.Forms
+{
+    partial class FingerprintRegistrationForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblStep;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Panel panelMain;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblStep = new System.Windows.Forms.Label();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.panelMain.SuspendLayout();
+            this.SuspendLayout();
+
+            // panelMain
+            this.panelMain.BackColor = System.Drawing.Color.White;
+            this.panelMain.Controls.Add(this.lblTitle);
+            this.panelMain.Controls.Add(this.lblStep);
+            this.panelMain.Controls.Add(this.btnScan);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(400, 250);
+            this.panelMain.TabIndex = 0;
+
+            // lblTitle
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(60, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(280, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Fingerprint Registration";
+
+            // lblStep
+            this.lblStep.AutoSize = true;
+            this.lblStep.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lblStep.Location = new System.Drawing.Point(80, 80);
+            this.lblStep.Name = "lblStep";
+            this.lblStep.Size = new System.Drawing.Size(230, 21);
+            this.lblStep.TabIndex = 1;
+            this.lblStep.Text = "Step 1 of 3: Scan Fingerprint";
+
+            // btnScan
+            this.btnScan.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnScan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnScan.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnScan.ForeColor = System.Drawing.Color.White;
+            this.btnScan.Location = new System.Drawing.Point(120, 140);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(150, 45);
+            this.btnScan.TabIndex = 2;
+            this.btnScan.Text = "Scan Finger";
+            this.btnScan.UseVisualStyleBackColor = false;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+
+            // FingerprintRegistrationForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.Controls.Add(this.panelMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FingerprintRegistrationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fingerprint Registration";
+            this.Load += new System.EventHandler(this.FingerprintRegistrationForm_Load);
+
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            this.ResumeLayout(false);
+        }
+    }
+}
