632eead6066b5d23feb70a61328f9edbc93822cc
eijei? <ajagodera2003@gmail.com>
2026-04-17 09:23:59 +0800

    Add files via upload

---
+++ Modules/DatabaseConnection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace BAMS.Modules
+{
+    public class DatabaseConnection
+    {
+        private static string connectionString =
+        "Server=Your_SERVER;Database=Your_DATABASE;User Id=Your_USER;Password=Your_PASSWORD;TrustServerCertificate=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
